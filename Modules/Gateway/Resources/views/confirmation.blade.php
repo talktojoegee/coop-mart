1a17b3d9edffab80fa32350ba618f746fca4ea40
talktojoegee <talktojoegee@gmail.com>
2023-04-12 08:39:46 +0100

    manual payment gateway test

--- Modules/Gateway/Resources/views/confirmation.blade.php
+++ Modules/Gateway/Resources/views/confirmation.blade.php
@@ -23,24 +23,6 @@
                 <p class="para-2">{{ formatNumber($purchaseData->total) }}</p>
             </div>
 
-            <div class="form-group mt-3">
-                <label for="">Payment Method</label>
-                <select name="paymentMethod" id="paymentMethod" class="form-control">
-                    <option disabled selected>-- Select payment method --</option>
-                    <option value="1">COOP Savings</option>
-                    <option value="2">COOP Loans</option>
-                </select>
-            </div>
-            <div class="form-group mt-3" id="loanPaymentMethod">
-                @if(!is_null($loanCollection))
-                    <button class="btn btn-primary">Proceed</button>
-                @endif
-            </div>
-            <div class="form-group mt-3" id="savingPaymentMethod">
-                @if(!is_null($savingsCollection))
-                    <button class="btn btn-primary">Proceed</button>
-                @endif
-            </div>
 
             <div>
                 <p class="para-1 text-end">{{ __('GATEAWAY') }}</p>
--- Modules/Gateway/Resources/views/pay.blade.php
+++ Modules/Gateway/Resources/views/pay.blade.php
@@ -1,7 +1,22 @@
 @extends('gateway::layouts.master')
 
 @section('content')
-    @forelse ($gateways as $gateway)
+    <a href="#" class="pay-box">
+        <div class="grow">
+            <img class="image-2" src="{{ asset('public/dist/img/paystack.png') }}" alt="{{ __('Image') }}" />
+        </div>
+    </a>
+    <a href="#" class="pay-box">
+        <div class="grow">
+            <img class="image-2" src="{{ asset('public/dist/img/coop.png') }}" alt="{{ __('Image') }}" />
+        </div>
+    </a>
+    <a href="#" class="pay-box">
+        <div class="grow">
+            <img class="image-2" src="{{ asset('public/dist/img/coop.png') }}" alt="{{ __('Image') }}" />
+        </div>
+    </a>
+   {{-- @forelse ($gateways as $gateway)
         <a href="{{ route('gateway.pay', withOldQueryIntegrity(['gateway' => $gateway->alias])) }}" class="pay-box">
             <div class="grow">
                 <img class="image-2" src="{{ asset(config($gateway->alias . '.logo')) }}" alt="{{ __('Image') }}" />
@@ -13,5 +28,5 @@
                 <h3>{{ __('No gateway found.') }}</h3>
             </div>
         </a>
-    @endforelse
+    @endforelse--}}
 @endsection
