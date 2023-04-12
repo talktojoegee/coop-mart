1722837a3ab984d0f3f4c99a15a7e9bc3319ba7c
talktojoegee <talktojoegee@gmail.com>
2023-04-12 09:00:58 +0100

    coop-savings confirmation init

--- Modules/Gateway/Routes/web.php
+++ Modules/Gateway/Routes/web.php
@@ -20,6 +20,7 @@ Route::group([
     'middleware' => ['locale']
 ], function () {
     Route::get('payment/confirmation', 'GatewayController@paymentConfirmation')->name('confirmation');
+    Route::get('payment/coop-savings', 'GatewayController@coopsavingsConfirmation')->name('coop_savings.confirmation');
     Route::get('payment/failed-payment', 'GatewayController@paymentFailed')->name('failed');
     Route::get('/payment', 'GatewayController@paymentGateways')->name('payment');
     Route::get('/payment/{gateway}/pay', 'GatewayController@pay')->name('pay');
