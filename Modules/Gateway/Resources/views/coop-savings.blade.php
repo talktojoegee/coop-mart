1206ccbe2422e02d0e5552f539aba4c95d4a0a9c
talktojoegee <talktojoegee@gmail.com>
2023-04-12 10:56:01 +0100

    Gateway process coop savings payment

--- Modules/Gateway/Resources/views/coop-savings.blade.php
+++ Modules/Gateway/Resources/views/coop-savings.blade.php
@@ -3,7 +3,16 @@
 @section('content')
     <div class="row">
         <div class="col-md-12">
-            <p>Transaction Successful</p>
+            <h5>COOP Savings</h5>
+            <form action="{{route('coop_savings.process-coop_savings_payment')}}" method="POST">
+                @csrf
+                <div class="form-group">
+                    <input type="hidden" name="memberId" value="{{Auth::user()->member_id}}">
+                    <input type="hidden" name="total" value="{{$purchaseData->total ?? 0 }}">
+                    <input type="hidden" name="code" value="{{$purchaseData->code ?? 0 }}">
+                    <button class="btn btn-primary btn-lg mt-2">Pay Now</button>
+                </div>
+            </form>
         </div>
     </div>
 @endsection
