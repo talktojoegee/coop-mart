5b8b71130ba530ae9fa9118cffafd9add67fc501
talktojoegee <talktojoegee@gmail.com>
2023-04-12 04:19:35 +0100

    COOP loan and savings assets

--- Modules/CoopLoan/Config/config.php
+++ Modules/CoopLoan/Config/config.php
@@ -3,5 +3,5 @@
 return [
     'name' => 'CoopLoan',
     'alias' => 'cooploan',
-    'logo' => 'Modules/CoopLoan/Resources/assets/savings.png',
+    'logo' => 'Modules/CoopLoan/Resources/assets/coop.png',
 ];
